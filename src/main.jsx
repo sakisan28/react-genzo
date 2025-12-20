dc5c7361a303ec3e9f18477059586e2dcab944cf
Takayuki Iwasaki <sakisan@st.rim.or.jp>
2025-12-21 00:45:07 +0900

    PWA

--- src/main.jsx
+++ src/main.jsx
@@ -1,3 +1,5 @@
+import { registerSW } from 'virtual:pwa-register'
+
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
@@ -8,3 +10,5 @@ createRoot(document.getElementById('root')).render(
     <App />
   </StrictMode>,
 )
+
+registerSW()
--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,29 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { VitePWA } from 'vite-plugin-pwa'
+
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: './',
   plugins: [
     react(),
     tailwindcss(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: '現像液調整計算機',
+        short_name: '現像液計算機',
+        description: 'フィルム現像液の希釈や温度調整を手助けする計算機です。',
+        theme_color: '#0ea5e9',
+        icons: [
+          {
+            src: 'nya-yama.png',
+            sizes: '512x512',}
+        ],
+        id: '/react-genzo/',
+      },
+    }),
   ],
 })
