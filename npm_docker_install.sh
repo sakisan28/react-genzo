d7a219c1950b1b9262a71c908718990e8d469035
Takayuki Iwasaki <sakisan@st.rim.or.jp>
2025-12-21 15:41:01 +0900

    docker sh remove

--- npm_docker_install.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-docker run -it --rm -v "$(pwd):/react-genzo" --workdir /react-genzo arm32v7/node npm install
--- npm_docker_run_dev.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-docker run -it --rm -v "$(pwd):/react-genzo" --workdir /react-genzo -p 5173:5173 arm32v7/node npm run dev
